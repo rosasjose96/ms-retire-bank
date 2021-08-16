2467332ad6895d19dfe5c9086c7d94bd0551b770
rosasjose96 <34173505+rosasjose96@users.noreply.github.com>
2021-08-16 15:53:23 -0500

    ADD loadbalance

--- src/main/java/com/example/msretire/config/WebClientConfig.java
+++ src/main/java/com/example/msretire/config/WebClientConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean(name = "client")
+    @LoadBalanced
     public WebClient.Builder registrarWebClient() {
         return WebClient.builder();
     }
